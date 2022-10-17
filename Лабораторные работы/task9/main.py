c44e38661cc5d33206da3ea20b81b382e2647bc7
Evgenii <115578536+Jel4qq@users.noreply.github.com>
2022-10-17 04:42:54 +0300

    Лабораторная работа 3

---
+++ Лабораторные работы/task9/main.py
@@ -0,0 +1,14 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+month = 0
+# TODO Оформить решение
+for month in range (0,10):
+    summa_ = spend - salary
+    spend = spend * 1.03
+    need_money = need_money + summa_
+    month += 1
+print(round(need_money))
